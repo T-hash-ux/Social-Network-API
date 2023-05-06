1851c1b6c0f48cb98d56a71b9fef6e116a20bc8c
Tyrrance <tyrrancem37@gmail.com>
2023-05-05 20:31:35 -0400

    Set this model to make user and thought available for use.

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// The purpose of this model is to make the User and Thought models available for use in other parts of the application by importing them from their respective files and exporting them as a single object.
 const User = require('./User');
 const Thought = require('./Thought');
 
